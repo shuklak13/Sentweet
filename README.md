4c0a264605cacc8b405d06c8ff12ac7de23e1ff8
Karan <shuklak13@gmail.com>
2016-01-10 19:12:39 -0600

    Readme
    Added a README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+SentweetSearch is a sentiment analysis application for Twitter data. It may classify tweets as positive or negative.
+
+Run the SentweetSearch.py file from the command line, as follows:
+
+python SentweetSearch.py
+
+The app will ask you for a search query. It will then return the 100 most recent public tweets with that keyword, as well as their detected sentiment, and the ratio of positive-to-negative tweets found.
--- SentweetSearch.py
+++ SentweetSearch.py
@@ -6,49 +6,53 @@
 			# http://ravikiranj.net/posts/2012/code/how-build-twitter-sentiment-analyzer/
 			# http://www.laurentluce.com/posts/twitter-sentiment-analysis-using-python-and-nltk/
 	#   Improve Sentiment Analysis
+	#		http://marcobonzanini.com/2015/05/17/mining-twitter-data-with-python-part-6-sentiment-analysis-basics/
 	#		movie sentiment analysis program
 	#		other classification algorithms
 	#		preprocessing, feature engineering
 #   Add back the location feature
 
-print "Booting up SentweetSearch..."
+#"main method"
+if __name__ == "__main__":
 
-#the essentials
-import sys
-sys.path.insert(0,"twitter")	#add to python's module search path
-import twitter
+	print "Booting up SentweetSearch..."
 
-#visuals
+	#the essentials
+	import sys
+	sys.path.insert(0,"twitter")	#add to python's module search path
+	import twitter
 
-#modules containing my functions
-from unicodeFunctions import *
-from trainClassifier import trainClassifier
-from getUserQuery import getUserQuery
-from twitterSearch import twitterSearch
-import visuals
+	#modules containing my functions
+	from unicodeFunctions import toUnicode, utf8
+	from trainClassifier import trainClassifier
+	from getUserQuery import getUserQuery
+	from twitterSearch import twitterSearch
+	import visuals
 
-classifier = trainClassifier()
+	classifier = trainClassifier()
 
-#GENERATE THIS STUFF USING THE TWITTER WEBSITE
-#CONSUMER_KEY=
-#CONSUMER_SECRET=
-#OAUTH_TOKEN=
-#OAUTH_TOKEN_SECRET=
+	#GENERATE THIS STUFF USING THE TWITTER WEBSITE
+	#If you need help, follow a tutorial:
+		#https://themepacific.com/how-to-generate-api-key-consumer-token-access-key-for-twitter-oauth/994/
+	#CONSUMER_KEY=
+	#CONSUMER_SECRET=
+	#OAUTH_TOKEN=
+	#OAUTH_TOKEN_SECRET=
 
-auth=twitter.oauth.OAuth(OAUTH_TOKEN,OAUTH_TOKEN_SECRET,CONSUMER_KEY,CONSUMER_SECRET)
-twitter.api = twitter.Twitter(auth=auth)
+	auth=twitter.oauth.OAuth(OAUTH_TOKEN,OAUTH_TOKEN_SECRET,CONSUMER_KEY,CONSUMER_SECRET)
+	twitter.api = twitter.Twitter(auth=auth)
 
-while True: #repeat forever!
-    query = getUserQuery()
+	while True: #repeat forever!
+	    query = getUserQuery()
 
-    searchResults = twitterSearch(query, classifier)
-    listOfTweets = searchResults[0]
-    numPos = searchResults[1]
-    numNeg = searchResults[2]
+	    searchResults = twitterSearch(query, classifier)
+	    listOfTweets = searchResults[0]
+	    numPos = searchResults[1]
+	    numNeg = searchResults[2]
 
-    visuals.createTables(listOfTweets)
-    visuals.createPieChart(numPos, numNeg, query)
+	    visuals.createTables(listOfTweets)
+	    visuals.createPieChart(numPos, numNeg, query)
 
-    continueCheck = raw_input("\nPress enter to search again, or type 'q' to quit.\n")
-    if continueCheck=="q":
-    	break
+	    continueCheck = raw_input("\nPress enter to search again, or type 'q' to quit.\n")
+	    if continueCheck=="q":
+	    	break
